257df13b3e1d00f245b09896845c940893ff5acb
JSHgithub <jinsheng@sqbj.com>
2015-06-06 10:55:27 +0800

    update code

---
+++ src/main/java/net/sf/cglib/proxy/SayHello.java
@@ -0,0 +1,13 @@
+package net.sf.cglib.proxy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: JSH
+ * Date: 2014/12/1
+ * Time: 15:03
+ */
+public class SayHello {
+    public void say() {
+        System.out.println("Hello everyone");
+    }
+}
